3560529fa86b18a7b48639e69f72e9a579308d37
Ml Anhem <m0345194302@gmail.com>
2025-08-15 21:16:01 +0700

    - update pooling

---
+++ Assets/Script/Environment/MaterialSpawner.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public class MaterialSpawner : MonoBehaviour
+{
+    [Header("Tên pool chứa các prefab")]
+    public string poolName = "Tree"; // Pool này có thể chứa nhiều prefab khác nhau
+
+    [Header("Điểm spawn trên map")]
+    public Transform[] spawnPoints;
+
+    [Header("Số lượng spawn mỗi lần")]
+    public int totalSpawnCount = 5; // Tổng số object muốn spawn mỗi lần
+    [Range(0f, 1f)]
+    public float firstPrefabRatio = 0.5f; // Tỷ lệ prefab 1 trong pool (còn lại là prefab 2)
+
+    [Header("Test trong Inspector")]
+    public bool spawnNow = false;
+
+    private void Update()
+    {
+        if (spawnNow)
+        {
+            spawnNow = false; // reset nút
+            SpawnMaterials();
+        }
+    }
+
+    public void SpawnMaterials()
+    {
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogWarning("Chưa gán điểm spawn!");
+            return;
+        }
+
+        if (totalSpawnCount > spawnPoints.Length)
+        {
+            Debug.LogWarning("Số lượng spawn vượt quá số điểm spawn!");
+            totalSpawnCount = spawnPoints.Length; // Giới hạn lại
+        }
+
+        // Tạo bản copy của spawnPoints và shuffle
+        Transform[] shuffledPoints = (Transform[])spawnPoints.Clone();
+        for (int i = 0; i < shuffledPoints.Length; i++)
+        {
+            int randIndex = Random.Range(i, shuffledPoints.Length);
+            var temp = shuffledPoints[i];
+            shuffledPoints[i] = shuffledPoints[randIndex];
+            shuffledPoints[randIndex] = temp;
+        }
+
+        int prefab1Count = Mathf.RoundToInt(totalSpawnCount * firstPrefabRatio);
+        int prefab2Count = totalSpawnCount - prefab1Count;
+
+        int pointIndex = 0;
+
+        // Spawn prefab thứ nhất (index = 0)
+        for (int i = 0; i < prefab1Count; i++)
+        {
+            ObjectPooler.Instance.GetFromPool(poolName, 0, shuffledPoints[pointIndex].position);
+            pointIndex++;
+        }
+
+        // Spawn prefab thứ hai (index = 1)
+        for (int i = 0; i < prefab2Count; i++)
+        {
+            ObjectPooler.Instance.GetFromPool(poolName, 1, shuffledPoints[pointIndex].position);
+            pointIndex++;
+        }
+    }
+
+}
---
+++ Assets/Script/Environment/ObjectPooler.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PrefabConfig
+{
+    public GameObject prefab;
+
+    [Header("Sorting")]
+    public bool autoSortByY = true;
+    public string sortingLayerName = "Default";
+    public int baseOrderInLayer = 0;
+    public Color defaultColor = Color.white;
+}
+
+[System.Serializable]
+public class PoolItem
+{
+    public string poolName;
+    public PrefabConfig[] prefabs; // Nhiều prefab + config sorting riêng
+    public int initialSize = 10;   // Đảm bảo có biến này cho khởi tạo pool
+    public bool expandable = true;
+}
+
+public class ObjectPooler : MonoBehaviour
+{
+    public static ObjectPooler Instance;
+
+    [Header("Danh sách pool")]
+    public List<PoolItem> itemsToPool;
+
+    private Dictionary<string, List<GameObject>> pooledObjects;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        pooledObjects = new Dictionary<string, List<GameObject>>();
+
+        // Khởi tạo tất cả pool
+        foreach (var item in itemsToPool)
+        {
+            List<GameObject> objectList = new List<GameObject>();
+
+            for (int i = 0; i < item.initialSize; i++)
+            {
+                int prefabIndex = i % item.prefabs.Length;
+                GameObject prefab = item.prefabs[prefabIndex].prefab;
+                if (prefab == null) continue;
+                GameObject obj = Instantiate(prefab);
+                obj.SetActive(false);
+                objectList.Add(obj);
+            }
+
+            pooledObjects.Add(item.poolName, objectList);
+        }
+    }
+
+    // Lấy object từ pool với chỉ số prefab cụ thể
+    public GameObject GetFromPool(string poolName, int prefabIndex, Vector3 position)
+    {
+        var itemConfig = itemsToPool.Find(x => x.poolName == poolName);
+        if (itemConfig == null || prefabIndex < 0 || prefabIndex >= itemConfig.prefabs.Length)
+            return null;
+
+        var list = pooledObjects[poolName];
+
+        foreach (var obj in list)
+        {
+            if (!obj.activeInHierarchy)
+            {
+                SetupObject(obj, position, itemConfig.prefabs[prefabIndex]);
+                return obj;
+            }
+        }
+
+        // Nếu không có object trống và được phép mở rộng
+        if (itemConfig.expandable)
+        {
+            GameObject prefab = itemConfig.prefabs[prefabIndex].prefab;
+            if (prefab == null) return null;
+            GameObject obj = Instantiate(prefab, position, Quaternion.identity);
+            SetupObject(obj, position, itemConfig.prefabs[prefabIndex]);
+            list.Add(obj);
+            return obj;
+        }
+
+        return null;
+    }
+
+    private void SetupObject(GameObject obj, Vector3 position, PrefabConfig config)
+    {
+        obj.transform.position = position;
+        obj.SetActive(true);
+
+        var sr = obj.GetComponent<SpriteRenderer>();
+        if (sr != null)
+        {
+            sr.sortingLayerName = config.sortingLayerName;
+            sr.color = config.defaultColor;
+
+            if (config.autoSortByY)
+                sr.sortingOrder = config.baseOrderInLayer + Mathf.RoundToInt(-position.y * 100);
+            else
+                sr.sortingOrder = config.baseOrderInLayer;
+        }
+    }
+
+    public void ReturnToPool(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+}
